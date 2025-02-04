8ca1dad9d0da6d66f10b809f1dcbab35a8f895b0
Akshey D <aksheyd@umich.edu>
2025-02-04 15:12:57 -0500

    rectangle

--- Game-Engine.cpp
+++ Game-Engine.cpp
@@ -103,28 +103,34 @@ int main()
 
     // triangles coordinates
     float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
     };
 
-    // STORE VERTICES IN GPU BUFFER
-    // vertex buffer (store buffer [triangle] on GPU for quick use)
-    unsigned int VBO, VAO;
-    // assign VBO AND VAO id for the GPU
-    glGenBuffers(1, &VBO);
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
-    // bind VBO AND VAO together
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    // bind VAO first
     glBindVertexArray(VAO);
-    // bind array buffer to VBO
+
+    // VBO buffer data
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // bind buffer to vertices (which is configurable via VBO)
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // TELL OPENGL HOW TO PROCESS VERTEX DATA
-    // location of vertex attr (set in vertex shader), size of vertex, type, whether its int/byte, stride size (each vec3 has 3 floats), void ptr for offset
+    // EBO buffer data
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // VAO attribute data
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    // now enable the vertex attr at location 0
     glEnableVertexAttribArray(0);
 
     // unbind VBO and unlink VAO
@@ -132,8 +138,8 @@ int main()
     glBindVertexArray(0);
 
 
-
-    
+    // wireframe
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 
 
@@ -146,10 +152,8 @@ int main()
 
         // now use created program
         glUseProgram(shaderProgram);
-        // bind to VAO
-        glBindVertexArray(VAO);
-        // draw triangle
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(VAO); 
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -158,6 +162,7 @@ int main()
     // deallocate mem
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
 
